09f06b54e3ce278dfb1c44b731780ba87ea71e99
Mykle Hansen <mykle-git@mykle.com>
2013-03-13 13:05:18 -0700

    still in progress ...

--- linker.sh
+++ linker.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/zsh 
+
+# install oh-my-zsh and janus:
+
+[ -d ~/.oh-my-zsh ] || git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+pushd ~
+[ -d ~/.vim ] || curl -Lo- https://bit.ly/janus-bootstrap | bash
+popd
 
 # link in all the dotfiles:
 
-DOTS=`ls -a -1 | tail -n+3 | grep -v .git`
+DOTS=`ls -ad -1 .??* | tail -n+3 | grep -v .git`
 echo linking $DOTS
 for i in $DOTS
 do
   [ -f ~/$i ] || [ -f $i ] && ln $i ~/$i && echo linked $i
 done
+
+# todo: link in ohmyzsh custom stuff.
