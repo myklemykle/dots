0e2a98b745111fa600ffc380f1bf41fbac49ea78
Mykle Hansen <mykle-git@mykle.com>
2013-03-21 09:58:08 -0700

    curse you janus

--- linker.sh
+++ linker.sh
@@ -2,20 +2,27 @@
 
 # install oh-my-zsh and janus:
 
-echo sorry, not working yet; exit
+#echo sorry, not working yet; exit
 
 [ -d ~/.oh-my-zsh ] || git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
 pushd ~
 [ -d ~/.vim ] || curl -Lo- https://bit.ly/janus-bootstrap | bash
 popd
 
 # link in all the dotfiles:
 
-DOTS=`ls -ad -1 .??* | tail -n+3 | grep -v .git`		# FAILING ... rethink.
+DOTS=`ls -ad -1 .??* | grep -v '^.git$'`		
 echo linking $DOTS
 for i in $DOTS
 do
   [ -f ~/$i ] || [ -f $i ] && ln $i ~/$i && echo linked $i
 done
 
-# todo: link in ohmyzsh custom stuff.
+####
+# don't really want host-specific stuff in here.  
+# maybe i will do a general-purpose 'custom' file later.
+#
+#  # link in ohmyzsh custom stuff.
+#  OHCUSS=~/.oh-my-zsh/custom   # ohmyzsh customization dir.
+#  [ -d "$OHCUSS" ] && ln ohmyzsh_custom/* "$OHCUSS"
