996c0d3a012bce13f6b12c5ec1f16859a295b966
Mykle Hansen <mykle@netx.net>
2022-02-17 11:34:57 -0800

    viv

---
+++ ohmyzsh_custom/viv.zsh
@@ -0,0 +1,10 @@
+# the new M1 mac with Big Sur ...
+#
+# M1 brew location:
+#
+# TODO: fix how PATH is handled ... local custom should mostly be here
+#export PATH="${PATH}:/opt/homebrew/bin"
+#
+# aliases for managing all my websites:
+. ~/Sites/aliases.zsh
+
